f68554da02ef8f92dd05a3b762177bca4fdfb0f5
Rendi Dwi Hermawan <rendiwithi@duck.www.tendawifi.com>
2022-03-29 20:30:20 +0700

    css dasar

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Cara Design Table Dengan CSS</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
- <h3>Tambah User<a class="right hide-on-med-and-down" href="{{URL('user/tambah')}}"><img src="assets/img/plus.png" width="100" height="80"/></a></h3>
-                    
-<center><h3>DAFTAR USER</h3></center>
-<table class="table1" align="center">
-<tr>
-<th>No</th>
-<th>userName</th>
-<th>Nama</th>
-<th>No Telpon</th>
-<th>Aksi</th>
-</tr>
 
-<?php $no=1; ?>
-@foreach ($user as $usr)
-<tr>
-<td>{{$no++}}</td>
-<td>{{$usr->username}}</td>
-<td>{{$usr->nama}}</td>
-<td>{{$usr->nohp}}</td>
-     <td>
-  <a href="/user/delete/{{$usr->id}}" onclick="return confirm('Admin Yakin ingin menghapusnya?');"><button class="btn waves-effect red white-text">Hapus</button></a>
-  <a href="/user/edit/{{$usr->id}}"><button class="btn waves-effect orange white-text">Edit</button></a>
-     </td>
-</tr>
-@endforeach
-</tbody>
-</table>
-</div>
+<head>
+   <title>E-Parkiran</title>
+   <link rel="stylesheet" type="text/css" href="/css/user.css">
+</head>
+
+<body>
+
+   <center>
+      <h3>DAFTAR USER</h3>
+   </center>
+   <!-- <h3>Tambah User<a class="right hide-on-med-and-down" href="{{URL('user/tambah')}}"><img src="assets/img/add.png" width="30" height="30" /></a></h3> -->
+   <a href="{{URL('user/tambah')}}" ><button  class="right hide-on-med-and-down">Tambah</button></a>
+   <table class="table1" align="center">
+      <tr>
+         <th>No</th>
+         <th>userName</th>
+         <th>Nama</th>
+         <th>No Telpon</th>
+         <th>Aksi</th>
+      </tr>
+      <?php $no = 1; ?>
+      @foreach ($user as $usr)
+      <tr>
+         <td>{{$no++}}</td>
+         <td>{{$usr->username}}</td>
+         <td>{{$usr->nama}}</td>
+         <td>{{$usr->nohp}}</td>
+         <td>
+            <a href="/user/delete/{{$usr->id}}" onclick="return confirm('Admin Yakin ingin menghapusnya?');"><button class="btn waves-effect red white-text">Hapus</button></a>
+            <a href="/user/edit/{{$usr->id}}"><button class="btn waves-effect orange white-text">Edit</button></a>
+         </td>
+      </tr>
+      @endforeach
+      </tbody>
+   </table>
+   </div>
